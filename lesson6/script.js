389d8f030b6da43d024f65a5c7f53d695a1df242
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-04-03 10:38:05 +0500

    git buyruqlari darsi

--- lesson6/script.js
+++ lesson6/script.js
@@ -1,1 +1,26 @@
 console.log("GitHub")
+
+// git init
+
+// git remote add origin https://ropezitoriy-manzili
+
+// gid add .
+
+// git commit -m "comentariya yoziladi"
+
+// git push origin master*
+
+// -------------------------------------------
+
+// Qo'shimcha so'rab qolsa:
+
+// git config --global user.email "abdurashidovabdurashidov630@gmail.com"
+// git config --global user.name "abdurashidovabdurashidov630-svg"
+
+// ------------------------------------------------------
+
+// Doim yangi code yozsak 3ta git buyruqini doim ishlatishimiz shart.
+
+// gid add .
+// git commit -m "comentariya yozing"
+// git push origin master*
