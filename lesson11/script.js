71a9062a77bc319160ab6c09b9604a490469a0db
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-04-17 09:55:51 +0500

    Function

---
+++ lesson11/script.js
@@ -0,0 +1,63 @@
+// Function
+
+// let a = 10
+// let b = 20
+// let c = 30
+
+// function maxSon(bir,ikki,uch){
+//     if(bir > ikki && bir > uch) console.log(bir, "katta");
+//     else if(ikki > bir && ikki > uch) console.log(ikki, "katta");
+//     else if(uch > ikki && uch > bir) console.log(uch, "katta");
+//     else console.log(bir, ikki, "va", uch, "Bular bir-biriga teng.")
+// }
+// maxSon(10,20,30)
+// maxSon(a,b,c)
+
+// let a = 1
+// let b = 2
+// let c = 3
+
+// function getValue(c,a,b){
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+
+// }
+// getValue(b,c,a)
+
+// let bir = 5;
+// let ikki = 3;
+// let uch = 1;
+// let tort = 2;
+// let besh = 4;
+
+// function getData(bir, ikki, uch, tort, besh) {
+//    console.log(ikki);
+//   console.log(bir);
+//   console.log(tort);
+//   console.log(uch);
+//   console.log(besh);
+// }
+// getData(ikki,besh,bir,tort,uch);
+
+// let a = 2;
+// let b = 1;
+// let c = 3;
+
+// function getResult(b,a,c,d) {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+//   console.log(d);
+// }
+// getResult(a+b,c*b,c/a,a-b+c);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let a = 10;
+let b = 20;
+function yigindi(raqam1, raqam2) {
+  let summa = raqam1 + raqam2;
+  return summa;
+}
+console.log(yigindi(a, b));
