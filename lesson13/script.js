e2c19cf7fd41f4752c22fb71a445547f1e47d0ec
abdurashidovabdurashidov630-svg <abdurashidovabdurashidov630@gmail.com>
2026-04-22 09:51:12 +0500

    Object

---
+++ lesson13/script.js
@@ -0,0 +1,46 @@
+// let a = 1;
+// let b = 2;
+// let c = 3;
+// function decloration(r1, r2, r3) {
+//   let summa = r1 + r2 + r3;
+//   return summa;
+// }
+// let sum = decloration(a, b, c);
+// console.log(sum / 2);
+
+// const expression = function(r1, r2, r3) {
+//   let summa = r1 + r2 + r3;
+//   return summa;
+// }
+// let sum2 = expression(a, b, c);
+// console.log(sum2 / 2);
+
+// const arrow = (r1, r2, r3) => {
+//   let summa = r1 + r2 + r3;
+//   return summa;
+// }
+// let sum3 = arrow(a, b, c);
+// console.log(sum3 / 2);
+
+//////////////////////////////////////////////
+
+// // OBJECT = {}
+
+// // 1. {}
+// // 2. Object()
+
+// let ozim = {
+//   ism: "Abdurahmon",
+//   familiya: "Abdurashidov",
+//   yosh: 13,
+//   maktabgaBoramanmi: true,
+//   manzil: "Namangan",
+//   telRaqam: "+998 88 111 56 16",
+// };
+// // console.log(ozim);
+
+// // 1. Static
+// console.log(ozim.ism, ozim.familiya);
+
+// // 2. Dinamic
+// console.log(ozim["ism"], ozim["familiya"]);
